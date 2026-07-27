df501965a7e219c8ca19fcc981befcaee6898968
Your Name <you@example.com>
2026-07-27 15:35:31 +0200

    refactor: rename touppercase function to printUpper and update return type

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: admin <admin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/21 17:27:48 by jchartie          #+#    #+#             */
-/*   Updated: 2026/07/27 11:45:05 by admin            ###   ########.fr       */
+/*   Updated: 2026/07/27 15:31:54 by admin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <cctype>
 #include <iostream>
 
-int touppercase(const std::string &str)
+void printUpper(const std::string &str)
 {
 	std::size_t i = 0;
 
@@ -24,17 +24,19 @@ int touppercase(const std::string &str)
 		std::cout << c;
 		i++;
 	}
-	return (0);
 }
 
 int main(int argc, char **argv)
 {
 	if (argc == 1)
-		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n", 0);
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return 0;
+	}
 	argv++;
 	while (*argv)
 	{
-		touppercase(*argv);
+		printUpper(*argv);
 		argv++;
 	}
 	std::cout << "\n";
