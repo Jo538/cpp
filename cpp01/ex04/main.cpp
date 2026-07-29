e4fece0e0c9cafcf68a904839b2a36847ee9606b
Your Name <you@example.com>
2026-07-29 18:24:14 +0200

    feat: add Makefile and main.cpp for file replacement functionality; include lyrics files

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jchartie <jchartie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/29 12:13:08 by jchartie          #+#    #+#             */
+/*   Updated: 2026/07/29 18:17:04 by jchartie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+
+void replaceLine(const std::string &buffer, std::ofstream &newFile, const std::string &s1, const std::string &s2)
+{
+	size_t pos = 0;
+
+	pos = buffer.find(s1);
+	if (pos == std::string::npos)
+	{
+		newFile << buffer;
+		return ;
+	}
+	newFile << buffer.substr(0, pos);
+	newFile << s2;
+	pos += s2.length();
+	replaceLine(buffer.substr(pos, buffer.length() - pos), newFile, s1, s2);
+}
+
+int ft_error(std::string str)
+{
+	std::cerr << str;
+	return 1; 
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+		return ft_error("Wrong number of arguments\n");
+
+	std::string buffer;
+	std::string fileIn = static_cast<std::string>(argv[1]);
+	std::string s1 = static_cast<std::string>(argv[2]);
+	std::string s2 = static_cast<std::string>(argv[3]);
+	std::string fileOut = fileIn + ".replace";
+	
+	std::ifstream oldFile(fileIn.c_str());
+	std::ofstream newFile(fileOut.c_str());
+	if (!oldFile || !newFile)
+		return ft_error(strerror(errno));
+
+	while (std::getline(oldFile, buffer))
+	{
+		replaceLine(buffer, newFile, s1, s2);
+		newFile << "\n";
+	}
+	return 0;
+}
