a5269fd32c3d159df4a1748532fda413addc804c
Your Name <you@example.com>
2026-08-04 20:34:00 +0200

    refactor: update main function to use triangle array for BSP tests

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: admin <admin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/31 14:17:47 by jchartie          #+#    #+#             */
-/*   Updated: 2026/08/04 18:31:13 by admin            ###   ########.fr       */
+/*   Updated: 2026/08/04 20:33:31 by admin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,22 @@
 
 int main( void ) 
 {
-	Point a(1, 1);
-	Point b(3, 9);
-	Point c(5, 4);
-	Point point(3, 4);
+	Point triangle[4][3] = {{Point(1, 2), Point(1, 2), Point(1, 2)},
+						   {Point(4, 9), Point(4, 9), Point(5, 4)},
+						   {Point(5, 4), Point(5, 4), Point(4, 9)},
+						   {Point(3, 4), Point(10, 10), Point(3, 4)}};
 
-	if (bsp(a, b, c, point))
-		std::cout << "point is in triangle\n";
-	else
-		std::cout << "point is outside triangle\n";
+	std::string test[3] = {"Test 1: a(1, 2), b(4, 9), c(5, 4), p(3, 4)\n",
+						   "\nTest 2: a(1, 2), b(4, 9), c(5, 4), p(10, 10)\n",
+						   "\nTest 3: a(1, 2), b(5, 4), c(4, 9), p(3, 4)\n"};
+	for (int i=0; i < 3; i++)
+	{
+		std::cout << test[i];
+		if (bsp(triangle[0][i], triangle[1][i], triangle[2][i], triangle[3][i]))
+			std::cout << "point is in triangle\n";
+		else
+			std::cout << "point is outside triangle\n";		
+	}
+
 	return 0;
 }
