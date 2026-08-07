29971ac250b49e9093ca93c72a0a4de759d0ec33
Your Name <you@example.com>
2026-08-07 16:25:53 +0200

    feat: add static constants for hit points, energy points, and attack damage in ClapTrap class

--- cpp03/ex02/main.cpp
+++ cpp03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: admin <admin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/05 15:30:30 by admin             #+#    #+#             */
-/*   Updated: 2026/08/06 20:07:37 by admin            ###   ########.fr       */
+/*   Updated: 2026/08/07 16:25:12 by admin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,24 @@
 
 int main(void)
 {
-	FragTrap player1("Yann");
-	
-	std::cout << player1 << std::endl;
+	// DEFAULT CONSTRUCTOR TEST
+	std::cout << "\033[1;32m----DEFAULT CONSTRUCTOR TEST----\033[0m\n";
+	FragTrap player1;
 
-	ClapTrap player2("Jo");
-	player2.attack("Yann");
+	// NAME CONSTRUCTOR TEST
+	std::cout << "\n\033[1;32m----NAME CONSTRUCTOR TEST----\033[0m\n";
+	FragTrap player2("Yann");
+
+	// COPY CONSTRUCTOR TEST
+	std::cout << "\n\033[1;32m----COPY CONSTRUCTOR TEST----\033[0m\n";
+	FragTrap player3(player1);
+
+	// ASSIGNMENT OPERATOR TEST
+	std::cout << "\n\033[1;32m----ASSIGNMENT OPERATOR TEST----\033[0m\n";
+	player3 = player2;
+
+	// HIGH FIVE() TEST
+	std::cout << "\n\033[1;32m----HIGH FIVE() TEST----\033[0m\n";
+	player2.highFiveGuys();
+	std::cout << std::endl;
 }
