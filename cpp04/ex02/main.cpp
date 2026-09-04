0c24f3b5c9bef8dc6d9c4e187547f45a359e05b7
Your Name <you@example.com>
2026-09-04 12:51:32 +0200

    fix: update output messages in copy constructor tests for Dog and Cat classes

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jchartie <jchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/07 19:46:31 by admin             #+#    #+#             */
-/*   Updated: 2026/09/04 12:19:12 by jchartie         ###   ########.fr       */
+/*   Updated: 2026/09/04 12:50:51 by jchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 int main(void)
 {
 	const int count = 4;
-	std::cout << BOLD << "\n----DEFAULT CONSTRUCTOR TESTS FOR AN ARRAY OF CATS & DOGS----\n" << COLOR_DEFAULT;
+	std::cout << BOLD << "----DEFAULT CONSTRUCTOR TESTS FOR AN ARRAY OF CATS & DOGS----\n" << COLOR_DEFAULT;
 	Animal *animal[count];
 	
 	for (int i = 0; i < (count / 2); i++)
@@ -49,7 +49,7 @@ int main(void)
 	catch(const std::out_of_range &e)
 	{
 		std::cerr << e.what() << std::endl;
-	}		
+	}	
 	
 	std::cout << BOLD << "\n----SET IDEA FOR CAT----\n" << COLOR_DEFAULT;
 	Cat sardine;
@@ -58,11 +58,15 @@ int main(void)
 	
 	std::cout << BOLD << "\n----COPY CONSTRUCTOR TESTS ON DOG----\n" << COLOR_DEFAULT;
 	Dog havana(pollux);
-	std::cout << havana.getIdea(0) << std::endl;
+	std::cout << "Copy constructor: Before idea changed: " << havana.getIdea(0) << std::endl;
+	havana.setIdea("I want cuddles! WAF WAF WAF!", 0);
+	std::cout << "Copy consturctor: After idea changed: " << havana.getIdea(0) << std::endl;
 
 	std::cout << BOLD << "\n----COPY CONSTRUCTOR TESTS ON CAT----\n" << COLOR_DEFAULT;
 	Cat salmon(sardine);
-	std::cout << salmon.getIdea(0) << std::endl;
+	std::cout << "Copy constructor: Before idea changed: " << salmon.getIdea(0) << std::endl;
+	salmon.setIdea("I want cuddles! MEOW MEOW MEOW!", 0);
+	std::cout << "Copy consturctor: After idea changed: " << salmon.getIdea(0) << std::endl;
 
 	// SET SECOND IDEA INTO DOG'S BRAIN	
 	havana.setIdea("I want to cuddle with my human! WAF WAF WAF!", 1);
